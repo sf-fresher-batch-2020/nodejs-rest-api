4255a3caa590930f96a2cebb8718ac1e547dff14
Naresh Kumar H <nareshkumarh@live.com>
2020-11-19 11:15:00 +0530

    Initial project

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 app.use(express.json())
-const port = process.env.port || 3000
+const port = process.env.port || 5000
 
 app.get("/", (req,res)=> res.send({message:"Working"}))
 
