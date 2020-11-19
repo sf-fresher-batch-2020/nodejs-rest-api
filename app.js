79de0a9311dc5835c7ba81e1279bdd6845d0d09d
Naresh Kumar H <nareshkumarh@live.com>
2020-11-19 15:20:39 +0530

    Added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
+const cors = require('cors');
 const TOKEN_SECRET = "7bc78545b1a3923cc1e1e19523fd5c3f20b409509";
 const app = express()
 app.use(cors())
